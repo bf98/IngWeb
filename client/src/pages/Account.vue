ffae2a1dd24d46aa420e84915da12a064b38dde9
bunnicula <bunnicula@DEBIAN-DESKTOP.broadband>
2025-04-03 16:19:04 +0200

    aggiornato Account.vue (da mettere a posto?)

--- client/src/pages/Account.vue
+++ client/src/pages/Account.vue
@@ -24,6 +24,26 @@
 </script>
 
 <template>
-	<p style="text-align: center;">Account Page</p>
-	<p style="text-align: center;" v-if="datiUser[0]">Your username is {{ datiUser[0].name }}</p>
+	<div class="account-page">
+		<div class="account-section">
+			<div v-if="datiUser[0]">
+				<div class="account-info">
+					<h2>Account</h2>
+					<p><strong>Username<br />Test {{user.name}}</strong></p>
+					<p><strong>Total Clicks<br />Test  {{user.click_num}}</strong></p>
+					<p><strong>Achievements<br />Test  {{user.achievements}}</strong></p>
+				</div>
+			<div class="flist-info">
+					<h1>Friend List <!--{{friend.list}}--></h1>
+				</div>
+		</div>
+		<div v-else>
+			<div class="account-header">
+				<h1>Account</h1>
+			</div>
+			<p><a href="/login">Login / Sign Up</a></p>
+		</div>
+		</div>
+	</div>
 </template>
+
