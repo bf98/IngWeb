d300bcd943a729ac12adaec5a49c5fdb1a194fed
bunnicula <>
2025-08-22 21:21:15 +0200

    aggiornato db .sql

--- backend/sql/zombie_clicker.sql
+++ backend/sql/zombie_clicker.sql
@@ -1,58 +1,109 @@
-/*M!999999\- enable the sandbox mode */ 
--- MariaDB dump 10.19-11.4.5-MariaDB, for debian-linux-gnu (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost    Database: zombie_clicker
--- ------------------------------------------------------
--- Server version	10.4.32-MariaDB
+-- Host: localhost
+-- Generation Time: Aug 22, 2025 at 09:20 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*M!100616 SET @OLD_NOTE_VERBOSITY=@@NOTE_VERBOSITY, NOTE_VERBOSITY=0 */;
+
+--
+-- Database: `zombie_clicker`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `achievements`
+--
+
+CREATE TABLE `achievements` (
+  `user_id` int(11) DEFAULT NULL,
+  `achievement_1` tinyint(1) DEFAULT 0,
+  `achievement_2` tinyint(1) DEFAULT 0,
+  `achievement_3` tinyint(1) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `achievements`
+--
+
+INSERT INTO `achievements` (`user_id`, `achievement_1`, `achievement_2`, `achievement_3`) VALUES
+(8, 0, 0, 0),
+(11, 0, 0, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `friends`
+--
+
+CREATE TABLE `friends` (
+  `user_id` int(11) DEFAULT NULL,
+  `friend_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `friends`
+--
+
+INSERT INTO `friends` (`user_id`, `friend_id`) VALUES
+(8, 11),
+(11, 8);
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL,
   `name` varchar(10) NOT NULL,
   `password` longtext DEFAULT NULL,
-  `click_num` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+  `click_num` bigint(20) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES
-(7,'sonic','$2b$10$Bk0q5WS284p0P.oeX/1IHOy2LFsnXjqPV3WubVXLvQ6uUO2z8MkG.',NULL),
-(8,'kyle','$2b$10$1TuReo3BemQg89.c0bTsVeo04Eh7Br8OnuZWlcAV8J2cinQd.lhlm',NULL),
-(9,'dale','$2b$10$7YFZyP0m55SclS0uMVnqHeGLfegDuanuykBg4fzmywUDj0Ce4cwK2',NULL),
-(10,'mike','$2b$10$IbqeyHxejnbrVn/WwHSxgOPUS3zPEuZ.sOWD9vPw/G51L/ZRObP8u',NULL);
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+INSERT INTO `users` (`id`, `name`, `password`, `click_num`) VALUES
+(8, 'kyle', '$2b$10$1TuReo3BemQg89.c0bTsVeo04Eh7Br8OnuZWlcAV8J2cinQd.lhlm', 1552),
+(11, 'bunnicula', '$2b$10$snKRZPUolP4O6MFkJn5Z7.qpex00DeZ9zsjz9rwZ0KoZ4VC.UJBOq', 1471),
+(12, 'foo', '$2b$10$Ia6ALX7QHvEwM9R5Bb2R1.DLpMcugeFgAkWiChDD9mHs3.r/xZmeO', 17);
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*M!100616 SET NOTE_VERBOSITY=@OLD_NOTE_VERBOSITY */;
-
--- Dump completed on 2025-03-25 18:31:37
